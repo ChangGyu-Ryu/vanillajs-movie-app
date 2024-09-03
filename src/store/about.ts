4efebce0ebd6debaebb0da04af1f56e5fe06219f
ChangGyu-Ryu <ckue21@gmail.com>
2024-09-03 10:17:39 +0900

    # About 스토어 타이핑, JSON to TS

--- src/store/about.ts
+++ src/store/about.ts
@@ -1,8 +1,16 @@
 import { Store } from "../core/ryucro";
-import Logo from "../images/logo.png";
+
+interface State {
+  photo: string
+  name: string
+  email: string
+  blog: string
+  github: string
+  repository: string
+}
 
 export default new Store({
-  photo: `'${Logo}'`,
+  photo: 'https://raw.githubusercontent.com/ChangGyu-Ryu/vanillajs-movie-app/main/src/images/logo.png',
   name: 'RYUCRO / RyuChangGyu',
   email: 'ckue21@gmail.com',
   blog: 'https://velog.io/@micaelknife/posts',
